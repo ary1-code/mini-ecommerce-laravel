c77b0d40a8b12cc244577c48dfcfaf9f5943c7cd
ary1-code <ariiyafallah9@gmail.com>
2025-10-07 14:07:34 +0330

    feat: implement basic authentication and UI layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="fa" dir="rtl">
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>
+        {{config('project.title')}}
+        @isset($title)
+            |
+            {{$title}}
+        @endisset
+    </title>
+
+    <link rel="stylesheet" href="{{asset('assets/styles/app.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/swiper/swiper.css')}}">
+
+
+    <!-- ==========================  DARK MODE SCRIPT ============================= -->
+    <script type="text/javascript">
+        if (
+            localStorage.theme === "dark" ||
+            (!("theme" in localStorage) &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    </script>
+</head>
+
+<body>
+
+<!-- ALL ICONS  -->
+@include('layouts.icons')
+
+<!-- Header -->
+@includeUnless(isset($rawLayout),'layouts.header')
+
+<!-- Slider -->
+@yield('content')
+<!-- Footer -->
+@includeUnless(isset($rawLayout),'layouts.footer')
+
+<!-- Overlay -->
+<div class="overlay"></div>
+<div class="search-overlay"></div>
+
+<script src="{{asset('assets/scripts/jquery-3.7.1.min.js')}}"></script>
+<script src="{{asset('assets/swiper/swiper.js')}}"></script>
+<script src="{{asset('assets/scripts/app.js')}}"></script>
+<script type="module" src="{{asset('assets/scripts/slider.js')}}"></script>
+
+
+</body>
+</html>
