c8ce0998e39e773e1e62d184a827b921595bf2e4
ary1-code <ariiyafallah9@gmail.com>
2025-10-11 21:39:27 +0330

    feat: Add helpers function

---
+++ app/helpers/helpers.php
@@ -0,0 +1,32 @@
+<?php
+if (!function_exists('getUserFullName')) {
+    function getUserFullName(): string
+    {
+
+        $user = auth()->user();
+
+        return "$user->first_name $user->last_name";
+
+    }
+
+}
+
+
+if (!function_exists('activeSideMenuItem')) {
+    function activeSideMenuItem(string $targetRoute): string
+    {
+        $activeClass = 'bg-blue-500/10 text-blue-500';
+        $defaultClass = 'hover:text-blue-500';
+        $currentRouteName = \Illuminate\Support\Facades\Route::currentRouteNamed();
+
+        if ($currentRouteName == $targetRoute) {
+
+            return $activeClass;
+
+        }
+
+        return $defaultClass;
+
+    }
+
+}
