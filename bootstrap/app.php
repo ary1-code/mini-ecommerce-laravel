f7f1f5acd1948915c51f70488987bcce99cab382
ary1-code <ariiyafallah9@gmail.com>
2025-11-07 15:34:32 +0330

    chore(bootstrap): configure middleware setup in application bootstrap file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,32 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: [
+            __DIR__ . '/../routes/web/user.php',
+            __DIR__ . '/../routes/web/admin.php'
+        ],
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(callback: function (Middleware $middleware): void {
+        $middleware->redirectGuestsTo(function () {
+            if (request()->routeIs('admin.*')) {
+                return route('admin.auth.login.index');
+            }
+
+            return route('auth.login.index');
+        });
+
+        $middleware->redirectUsersTo(function () {
+            if (Auth::guard('admin')->check()) {
+                return route('admin.dashboard');
+            }
+            return route('account.profile.index');
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
