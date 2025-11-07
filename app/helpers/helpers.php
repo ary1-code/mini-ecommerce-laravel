803fdcb693624bfe7cf2375c84241be7aea628ff
ary1-code <ariiyafallah9@gmail.com>
2025-11-07 15:29:36 +0330

    feat(core): add global helpers and set default filesystem disk to public

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Product;
+use App\Models\User;
 use App\Services\UserCartManager;
 
 if (!function_exists('getUserFullName')) {
@@ -11,6 +12,19 @@ if (!function_exists('getUserFullName')) {
 
         return "$user->first_name $user->last_name";
 
+    }
+
+}
+
+
+if (!function_exists('getUserName')) {
+    function getUserName(User|null $user = null): string
+    {
+        if (!$user) {
+            $user = auth()->user();
+
+        }
+        return " $user->first_name $user->last_name";
     }
 
 }
@@ -26,8 +40,6 @@ if (!function_exists('getAdminFullName')) {
     }
 
 }
-
-
 
 if (!function_exists('activeSideMenuItem')) {
     function activeSideMenuItem(string $targetRoute): string
@@ -65,48 +77,73 @@ if (!function_exists('activeSideMenuItem')) {
     }
 
 
-if (!function_exists('getFullProductName')) {
-    function getFullProductName(Product $product): string
-    {
-
-        return $product->name . ' | ' . $product->name_en;
-
-    }
-}
-
-if (!function_exists('getDiscountPercent')) {
-    function getDiscountPercent(Product $product): int
-    {
-        return (int)(($product->disscount * 100) / $product->price);
-
-    }
-}
-
-if (!function_exists('generateSortColorClass')) {
-    function generateSortColorClass(string $current): string
-    {
-        if (!request()->filled('sort') && $current == 'newest') {
-
-            return 'text-blue-500';
-        }
-
-        $sortFromQs = request()->input('sort');
-
-        if ($sortFromQs == $current) {
-
-            return 'text-blue-500';
-        }
-
-        return "text-gray-400";
-
-    }
-
-    if (!function_exists('getUserProductCount')) {
-        function getUserProductCount(): int
+    if (!function_exists('getFullProductName')) {
+        function getFullProductName(Product $product): string
         {
-            return UserCartManager::getProductCount();
+
+            return $product->name . ' | ' . $product->name_en;
 
         }
+    }
+
+    if (!function_exists('getDiscountPercent')) {
+        function getDiscountPercent(Product $product): int
+        {
+            return (int)(($product->disscount * 100) / $product->price);
+
+        }
+    }
+
+    if (!function_exists('generateSortColorClass')) {
+        function generateSortColorClass(string $current): string
+        {
+            if (!request()->filled('sort') && $current == 'newest') {
+
+                return 'text-blue-500';
+            }
+
+            $sortFromQs = request()->input('sort');
+
+            if ($sortFromQs == $current) {
+
+                return 'text-blue-500';
+            }
+
+            return "text-gray-400";
+
+        }
+
+        if (!function_exists('getUserProductCount')) {
+            function getUserProductCount(): int
+            {
+                return UserCartManager::getProductCount();
+
+            }
+        }
+
+        if (!function_exists('enumToValue')) {
+            function enumToValue(string $enumClass): mixed
+            {
+
+                $values = [];
+
+                foreach ($enumClass::cases() as $case) {
+                    $values[] = $case->value;
+                }
+                return $values;
+            }
+        }
+    }
+    function getProductDefaultImageUrl(Product $product): ?string
+    {
+
+        $defaultImage = $product->defaultImage()->with('file')->first();
+
+        if ($defaultImage && $defaultImage->file) {
+            return asset('storage/' . $defaultImage->file->path);
+        }
+
+        return null;
     }
 }
 
